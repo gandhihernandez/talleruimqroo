c2e3a46cf081fa9ef070947e2cb80bd820792711
gandhihernandez <gandhi.hernandez@gmail.com>
2023-11-28 18:58:22 -0600

    Add files via upload

---
+++ Material Adicional/escuelas.js
@@ -0,0 +1,33 @@
+var escuela = { 'type':'FeatureCollection', 
+'features': [ 
+ {'type':'Feature', 
+ 'properties':{'nombre':'Escuela Primaria Laura Méndez de Cuenca'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-89.6449511,21.0428409]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Escuela Primaria Zamná'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-89.6255788,21.0370622]}},
+ {'type':'Feature', 
+ 'properties':{'nombre':'Escuela Primaria Ana Sofía Baeza García vespertino'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-89.7087103,20.996205]}} 
+]}; 
+var dia = { 'type':'FeatureCollection', 
+'features': [ 
+ {'type':'Feature', 
+ 'properties':{'respuesta':'El día'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.0827003,19.4464479]}},
+ {'type':'Feature', 
+ 'properties':{'respuesta':'El día'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.1749605,19.4293039]}},
+ {'type':'Feature', 
+ 'properties':{'respuesta':'El día'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.1147913,19.4300609]}},
+ {'type':'Feature', 
+ 'properties':{'respuesta':'El día'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.1920307,19.4194815]}},
+ {'type':'Feature', 
+ 'properties':{'respuesta':'El día'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.1891748,19.3228012]}},
+ {'type':'Feature', 
+ 'properties':{'respuesta':'El día'}, 
+ 'geometry': {'type':'Point', 'coordinates':[-99.1004137,19.4059402]}} 
+]};
